fee1582668c31c994471403b22af6edef2a8e327
Ankit Joshi <ankit.joshi3011@gmail.com>
2024-11-19 21:35:01 +0530

    Updated module-info.java to require the correct dependencies and export the correct packages

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,15 @@
 module com.jmc.mazebank {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires de.jensd.fx.glyphs.fontawesome;
+    requires java.sql;
+    requires org.xerial.sqlitejdbc;
 
     opens com.jmc.mazebank to javafx.fxml;
     exports com.jmc.mazebank;
+    exports com.jmc.mazebank.controllers;
+    exports com.jmc.mazebank.controllers.admin;
+    exports com.jmc.mazebank.controllers.client;
+    exports com.jmc.mazebank.models;
+    exports com.jmc.mazebank.views;
 }
