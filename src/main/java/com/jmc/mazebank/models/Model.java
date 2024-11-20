6ada983eb17f6ceb15231ee60f1137a7b3698340
Ankit Joshi <ankit.joshi3011@gmail.com>
2024-11-20 18:51:46 +0530

    Made the Model class a Singleton

--- src/main/java/com/jmc/mazebank/models/Model.java
+++ src/main/java/com/jmc/mazebank/models/Model.java
@@ -1,4 +1,16 @@
 package com.jmc.mazebank.models;
 
 public class Model {
+    private static Model model;
+
+    private Model() {
+    }
+
+    public static synchronized Model getInstance() {
+        if (model == null) {
+            model = new Model();
+        }
+
+        return model;
+    }
 }
