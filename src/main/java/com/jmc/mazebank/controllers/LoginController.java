17c46af9bddde741113d8f4938a97aa5f5e2f27a
Ankit Joshi <ankit.joshi3011@gmail.com>
2024-11-20 14:05:23 +0530

    Added the FXML file, CSS file, and the Controller file for the Login page

--- src/main/java/com/jmc/mazebank/controllers/LoginController.java
+++ src/main/java/com/jmc/mazebank/controllers/LoginController.java
@@ -1,4 +1,27 @@
 package com.jmc.mazebank.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    @FXML
+    private ChoiceBox acc_selector;
+
+    @FXML
+    private Label payee_address_label;
+
+    @FXML
+    private TextField payee_address_field;
+
+    @FXML
+    private TextField password_field;
+
+    @FXML
+    private Button login_button;
+
+    @FXML
+    private Label error_label;
 }
