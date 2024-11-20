97d2431bc7815db925eb6d7e949510d0a14349d8
Ankit Joshi <ankit.joshi3011@gmail.com>
2024-11-20 18:34:11 +0530

    Made the LoginController implement the Initializable interface

--- src/main/java/com/jmc/mazebank/controllers/LoginController.java
+++ src/main/java/com/jmc/mazebank/controllers/LoginController.java
@@ -1,12 +1,16 @@
 package com.jmc.mazebank.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
     @FXML
     private ChoiceBox acc_selector;
 
@@ -24,4 +28,8 @@ public class LoginController {
 
     @FXML
     private Label error_label;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
 }
