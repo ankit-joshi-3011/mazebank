ffc76e36f72ef77fd17ae744b7a23709bed8c5e7
Ankit Joshi <ankit.joshi3011@gmail.com>
2024-11-21 14:35:28 +0530

    Added the FXML file, CSS file, and the Controller file for the Client Cell

---
+++ src/main/java/com/jmc/mazebank/controllers/admin/ClientCellController.java
@@ -0,0 +1,28 @@
+package com.jmc.mazebank.controllers.admin;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class ClientCellController {
+    @FXML
+    private Label first_name_label;
+
+    @FXML
+    private Label last_name_label;
+
+    @FXML
+    private Label payee_address_label;
+
+    @FXML
+    private Label savings_account_first_label;
+
+    @FXML
+    private Label savings_account_second_label;
+
+    @FXML
+    private Label date_label;
+
+    @FXML
+    private Button delete_button;
+}
