a3c788245c4fa776d4f5be64fe655188e23d65be
Ankit Joshi <ankit.joshi3011@gmail.com>
2024-11-21 12:55:55 +0530

    Added the FXML file, CSS file, and the Controller file for the Clients view

---
+++ src/main/java/com/jmc/mazebank/controllers/admin/ClientsController.java
@@ -0,0 +1,18 @@
+package com.jmc.mazebank.controllers.admin;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientsController implements Initializable {
+    @FXML
+    private ListView clients_listview;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        
+    }
+}
