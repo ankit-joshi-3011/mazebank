3c48c59ec6247b87c389486898b7dcd250cc0057
Ankit Joshi <ankit.joshi3011@gmail.com>
2024-11-22 14:13:18 +0530

    Auto-formatted App.java

--- src/main/java/com/jmc/mazebank/App.java
+++ src/main/java/com/jmc/mazebank/App.java
@@ -5,12 +5,12 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class App extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
     @Override
     public void start(Stage stage) {
         ViewFactory.getInstance().showLoginWindow();
-    }
-
-    public static void main(String[] args) {
-        launch();
     }
 }
