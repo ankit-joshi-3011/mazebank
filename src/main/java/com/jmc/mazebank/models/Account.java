185ea75a2e2c73eac2e6158a813d86a007c3bfb6
Ankit Joshi <ankit.joshi3011@gmail.com>
2024-11-22 16:29:17 +0530

    - Starting the addition of the date created field in the database. - Changed the name of SavingsAccountFirst table to SavingsAccount and SavingsAccountSecond table to PensionAccount in the database.

--- src/main/java/com/jmc/mazebank/models/Account.java
+++ src/main/java/com/jmc/mazebank/models/Account.java
@@ -1,19 +1,20 @@
 package com.jmc.mazebank.models;
 
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+import javafx.beans.property.*;
+
+import java.time.LocalDate;
 
 public abstract class Account {
     private final StringProperty owner;
     private final StringProperty accountNumber;
     private final DoubleProperty balance;
+    private final ObjectProperty<LocalDate> dateCreated;
 
-    public Account(String owner, String accountNumber, Double balance) {
+    public Account(String owner, String accountNumber, Double balance, LocalDate dateCreated) {
         this.owner = new SimpleStringProperty(this, "owner", owner);
         this.accountNumber = new SimpleStringProperty(this, "accountNumber", accountNumber);
         this.balance = new SimpleDoubleProperty(this, "balance", balance);
+        this.dateCreated = new SimpleObjectProperty<>(this, "dateCreated", dateCreated);
     }
 
     public StringProperty ownerProperty() {
@@ -26,6 +27,10 @@ public abstract class Account {
 
     public DoubleProperty balanceProperty() {
         return balance;
+    }
+
+    public ObjectProperty<LocalDate> dateCreatedProperty() {
+        return dateCreated;
     }
 
     public void setBalance(double amount, boolean addOrSubtract) {
--- src/main/java/com/jmc/mazebank/models/SavingsAccount.java
+++ src/main/java/com/jmc/mazebank/models/SavingsAccount.java
@@ -3,11 +3,13 @@ package com.jmc.mazebank.models;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 
+import java.time.LocalDate;
+
 public class SavingsAccount extends Account {
     private final IntegerProperty transactionLimit;
 
-    public SavingsAccount(String owner, String accountNumber, Double balance, Integer transactionLimit) {
-        super(owner, accountNumber, balance);
+    public SavingsAccount(String owner, String accountNumber, Double balance, LocalDate dateCreated, Integer transactionLimit) {
+        super(owner, accountNumber, balance, dateCreated);
         this.transactionLimit = new SimpleIntegerProperty(this, "transactionLimit", transactionLimit);
     }
 
